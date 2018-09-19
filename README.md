68dc6d13da007821c4278e19360e8c018c6393d5
Puigdomenech <Miquel.Puigdomenech@dnvgl.com>
2018-09-19 12:19:16 +0200

    added aditional exercise, also added link to exercism profile with path exercises

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 extra exercises from exercism fsharp track for work-coleagues comparation 
 
 Since the extra exercises don't usually get approved this is my interpretation of the solution but its not verified 
+
+The path exercises can be found here -> (https://exercism.io/profiles/hiroCat)
---
+++ grains/Grains.fs
@@ -0,0 +1,15 @@
+﻿module Grains
+
+let square (n: int): Result<uint64,string> = 
+    match n with 
+    | n when n > 64 || n < 1 -> Result.Error "Invalid input"
+    | _ -> Result.Ok (uint64 (2.0**((float)n-1.0)))
+
+let total: Result<uint64,string> = 
+    Result.Ok( [0..64] 
+                |> Seq.sumBy(fun x -> (x 
+                                        |> square 
+                                        |> function Ok r -> r 
+                                                    | Error r -> (uint64 0))))
+                                   
+ 
